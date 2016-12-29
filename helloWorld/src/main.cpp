98d7f4ac61bcd689eaf9209e2d2453c60860b862
vladislav.pilianski <needforgrib@mail.ru>
2016-12-29 18:17:48 +0300

    implement /rooms and /rooms/id

--- helloWorld/src/main.cpp
+++ helloWorld/src/main.cpp
@@ -1,31 +1,93 @@
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <stdlib.h>
+#include <regex>
+#include <stdlib.h>
+#include <ctype.h>
 #include "fcgio.h"
 
 using namespace std;
 
-struct Room {
-	string id;
-	float oxygen_amount;
-	float light;
-	bool opened;
-	int personal_capacity;
-	float water_amount;
-	string type;
+string toString(float a) {
+    ostringstream temp;
+    temp << a;
+    return temp.str();
+}
 
-	string toJson() {
-		return "{ id: " + id + ", light:"; 
-//			", opened:" + opened + ", oxygen_amount:" + 
-//			oxygen_amount + ", personal_capacity:" + 
-//			personal_capacity + ", water_amount:" +
-//			water_amount + ", type:" + type + "}";  	
-	}	
+string toString(int a) {
+    ostringstream temp;
+    temp << a;
+    return temp.str();
+}
+
+string toString(bool a) {
+    ostringstream temp;
+    temp << a;
+    return temp.str();
+}
+
+
+struct Room {
+    string id;
+    float oxygen_amount;
+    float light;
+    bool opened;
+    int personal_capacity;
+    float water_amount;
+    string type;
+
+    string toJson() {
+        return "{ id:" + id + ", light:" + toString(light) +
+               ", opened:" + toString(opened) + ", oxygen_amount:" +
+                toString(oxygen_amount) + ", personal_capacity:" +
+                toString(personal_capacity) + ", water_amount:" +
+                toString(water_amount) + ", type:" + type + "}";
+    }
 };
 
+
+Room *rooms;
+
+const int N = 10;
+
+string roomsJson(Room *rooms) {
+    string result = "{ rooms: [";
+    for (int i = 0; i < N; ++i) {
+        result += rooms[i].toJson();
+        if (i != N -1) {
+            result += ",";
+        }
+    }
+    result += "]}";
+    return result;
+}
+
+Room getRoomById(Room *rooms, string id) {
+    for (int i = 0; i < N; ++i) {
+        if (rooms[i] == id) {
+            return rooms[i];
+        }
+    }
+    return NULL;
+}
+
+void fillRooms() {
+    for (int i = 0; i < N; ++i) {
+        rooms[i].id = toString(i);
+        rooms[i].oxygen_amount = (float) rand() / RAND_MAX;
+        rooms[i].light = (float) rand() / RAND_MAX;
+        rooms[i].water_amount = (float) rand() / RAND_MAX;
+    }
+}
+
 int main(void) {
-    streambuf * cin_streambuf  = cin.rdbuf();
-    streambuf * cout_streambuf = cout.rdbuf();
-    streambuf * cerr_streambuf = cerr.rdbuf();
+    rooms = new Room[N];
+    fillRooms();
+
+    streambuf *cin_streambuf = cin.rdbuf();
+    streambuf *cout_streambuf = cout.rdbuf();
+    streambuf *cerr_streambuf = cerr.rdbuf();
 
     FCGX_Request request;
 
@@ -41,18 +103,21 @@ int main(void) {
         cout.rdbuf(&cout_fcgi_streambuf);
         cerr.rdbuf(&cerr_fcgi_streambuf);
 
-	const char* uri = FCGX_GetParam("REQUEST_URI", request.envp);
+        const char *uri = FCGX_GetParam("REQUEST_URI", request.envp);
 
-        cout << "Content-type: text/html\r\n"
-             << "\r\n"
-             << "<html>\n"
-             << "  <head>\n"
-             << "    <title>Hello, World!</title>\n"
-             << "  </head>\n"
-             << "  <body>\n"
-             << "    <h1>Hello, " << uri <<" </h1>\n"
-             << "  </body>\n"
-             << "</html>\n";
+        regex regexTemplate("\\b(\\rooms\\)([^ ]*)");
+        char*match = new char[100];
+
+        if (uri == "/rooms") {
+            cout << roomsJson(rooms);
+        } else if (regex_search(uri, match, regexTemplate)) {
+            Room room = getRoomById(rooms, match);
+            if (room != NULL) {
+                cout << room.toJson();
+            } else {
+
+            }
+        }
     }
 
     cin.rdbuf(cin_streambuf);
