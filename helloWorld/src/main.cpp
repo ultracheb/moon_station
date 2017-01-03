3927714826a5618149d3b3ed45c02e58bbf0395f
user <user@ubuntu.(none)>
2017-01-04 01:47:17 +0300

    fix regex error

--- helloWorld/src/main.cpp
+++ helloWorld/src/main.cpp
@@ -7,11 +7,18 @@
 #include <ctype.h>
 #include "fcgio.h"
 
+#include <cstdint>
+#include <vector>
+#include <bsoncxx/json.hpp>
+#include <mongocxx/client.hpp>
+#include <mongocxx/stdx.hpp>
+#include <mongocxx/uri.hpp>
 #include <bsoncxx/builder/stream/document.hpp>
 #include <bsoncxx/json.hpp>
-
-#include <mongocxx/client.hpp>
 #include <mongocxx/instance.hpp>
+
+using bsoncxx::builder::stream::document;
+using bsoncxx::builder::stream::finalize;
 
 using namespace std;
 
@@ -39,31 +46,31 @@ string getRoomById(char * id) {
     return result;
 }
 
-string get_request_content(const FCGX_Request & request) {
-    char * content_length_str = FCGX_GetParam("CONTENT_LENGTH",
-                                               request.envp);
-    unsigned long content_length = STDIN_MAX;
+//string get_request_content(const FCGX_Request & request) {
+//    char * content_length_str = FCGX_GetParam("CONTENT_LENGTH",
+//                                               request.envp);
+//    unsigned long content_length = STDIN_MAX;
 
-    if (content_length_str) {
-        content_length = strtol(content_length_str,
-                                &content_length_str,
-                                10);
+  //  if (content_length_str) {
+    //    content_length = strtol(content_length_str,
+ //                              &content_length_str,
+      //                          10);
 
-        if (*content_length_str) {
-            cerr << "Can't Parse 'CONTENT_LENGTH='"
-                 << FCGX_GetParam("CONTENT_LENGTH", request.envp)
-                 << "'. Consuming stdin up to " << STDIN_MAX << endl;
-        }
+        //if (*content_length_str) {
+        //    cerr << "Can't Parse 'CONTENT_LENGTH='"
+          //       << FCGX_GetParam("CONTENT_LENGTH", request.envp)
+            //     << "'. Consuming stdin up to " << STDIN_MAX << endl;
+        //}
 
-        if (content_length > STDIN_MAX) {
-            content_length = STDIN_MAX;
-        }
-    } else {
+        //if (content_length > STDIN_MAX) {
+        //    content_length = STDIN_MAX;
+        //}
+    //} else {
         // Do not read from stdin if CONTENT_LENGTH is missing
-        content_length = 0;
-    }
-    ...
-}
+      //  content_length = 0;
+    //}
+    //return "";
+//}
 
 int main(void) {
     streambuf *cin_streambuf = cin.rdbuf();
@@ -84,16 +91,16 @@ int main(void) {
         cout.rdbuf(&cout_fcgi_streambuf);
         cerr.rdbuf(&cerr_fcgi_streambuf);
 
-        const char *uri = FCGX_GetParam("REQUEST_URI", request.envp);
+        char *uri = FCGX_GetParam("REQUEST_URI", request.envp);
 	string result = "empty";
-        regex regexTemplate("\\b(\\rooms\\)([^ ]*)");
-        char*match = new char[100];
+        regex regexTemplate("\\b(rooms/)([^ ]*)");
+        smatch match;
+	
 	if (strcmp(uri, "/rooms") == 0) {
             result = getAllRooms();
+        } else if (regex_search((string)(uri + 1), match, regexTemplate)) {
+            result = getRoomById(uri + 7);
         }
-	 else if (regex_search(uri, match, regexTemplate)) {
-            result = getRoomById(match);
-         }
 	cout << "Content-type: bson\r\n" 
 	<< "\r\n" << result << "\n";
     }
