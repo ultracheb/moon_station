12b3d6dad3f2290c7911585cf43e466342b89245
user <user@ubuntu.(none)>
2017-01-03 06:09:32 +0300

    delete useless

--- helloWorld/src/main.cpp
+++ helloWorld/src/main.cpp
@@ -15,60 +15,6 @@
 
 using namespace std;
 
-string toString(float a) {
-    ostringstream temp;
-    temp << a;
-    return temp.str();
-}
-
-string toString(int a) {
-    ostringstream temp;
-    temp << a;
-    return temp.str();
-}
-
-string toString(bool a) {
-    ostringstream temp;
-    temp << a;
-    return temp.str();
-}
-
-
-struct Room {
-    string id;
-    float oxygen_amount;
-    float light;
-    bool opened;
-    int personal_capacity;
-    float water_amount;
-    string type;
-
-    string toJson() {
-        return "{ id:" + id + ", light:" + toString(light) +
-               ", opened:" + toString(opened) + ", oxygen_amount:" +
-                toString(oxygen_amount) + ", personal_capacity:" +
-                toString(personal_capacity) + ", water_amount:" +
-                toString(water_amount) + ", type:" + type + "}";
-    }
-};
-
-
-Room *rooms;
-
-const int N = 10;
-
-string roomsJson(Room *rooms) {
-    string result = "{ rooms: [";
-    for (int i = 0; i < N; ++i) {
-        result += rooms[i].toJson();
-        if (i != N -1) {
-            result += ",";
-        }
-    }
-    result += "]}";
-    return result;
-}
-
 mongocxx::instance inst{};
 mongocxx::client conn{mongocxx::uri{}};
 
@@ -104,23 +50,16 @@ int main(void) {
 
         const char *uri = FCGX_GetParam("REQUEST_URI", request.envp);
 	string result = "empty";
-	//cout << uri << "\n";
-	//cout << "noup";
         //regex regexTemplate("\\b(\\rooms\\)([^ ]*)");
         //char*match = new char[100];
 	if (strcmp(uri, "/rooms") == 0) {
             result = dbWork();
-        }// else{cout << "nop";}// else if (regex_search(uri, match, regexTemplate)) {
+        }
+	// else if (regex_search(uri, match, regexTemplate)) {
         //    Room room = getRoomById(rooms, match);
-        //    if (room != NULL) {
-        //        cout << room.toJson();
-        //    } else {
-         //       cout << "{ status: 404}";
-        //    }
-        //}
-	cout << "Content-type: text/html\r\n" 
+        // }
+	cout << "Content-type: bson\r\n" 
 	<< "\r\n" << result << "\n";
-        
     }
 
     cin.rdbuf(cin_streambuf);
